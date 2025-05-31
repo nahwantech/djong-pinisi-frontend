c76585720021949d24ef973ac6a08053c1959113
Rizki Sadewa <rizki.sadewa@pegadaian.co.id>
2025-05-31 22:40:06 +0700

    init git

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+ 
+// These styles apply to every route in the application
+import './globals.css'
+ 
+export const metadata: Metadata = {
+  title: 'Djong Pinisi Apps',
+  description: 'Power your travel moment.',
+}
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
