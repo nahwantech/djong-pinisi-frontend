8787dd5c174b83ad689790b931396f190e1cebb2
Rizki Sadewa <rizki.sadewa@pegadaian.co.id>
2025-09-16 08:49:44 +0700

    fixing loading page, change to circle spanner

--- components/sales-pipeline/SalesPipelineInputModal.tsx
+++ components/sales-pipeline/SalesPipelineInputModal.tsx
@@ -87,13 +87,12 @@ const SalesPipelineInputModal: React.FC<SalesPipelineInputModalProps> = ({
 
                         {/* Select Status */}
                         <div className="mt-4 mb-4">
-                            <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">Filter by stage:</label>
+                            <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">Stage:</label>
                             <select
                                 id="filter"
                                 onChange={(e) => onSelectStatus(e.target.value)}
                                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                             >
-                                <option value="">All</option>
                                 <option value="Initial Contact">Initial Contact</option>
                                 <option value="Qualification">Qualification</option>
                                 <option value="Proposal">Proposal</option>
